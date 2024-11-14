5fdcd425b11dffabc9113661fa84802fd66d941a
oxypetals <157205437+oxypetals@users.noreply.github.com>
2024-11-14 17:59:10 +0100

    Update app.py

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,152 +1,82 @@
-import os
 from flask import Flask, request, Response, send_from_directory, render_template, after_this_request
+import os
 import ebooklib
 from ebooklib import epub
 from datetime import datetime
 
-
-UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-# DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
-
 app = Flask(__name__)
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+# Configure upload directory
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)  # Ensure upload folder exists
 
 @app.route("/")
 def index():
-    # if(ori_title+'_modified'+'.epub' in app.config['UPLOAD_FOLDER']):
-    #     os.remove(app.config['UPLOAD_FOLDER']+'/'+ori_title+'_modified'+'.epub')
     return render_template('index.html')
 
-
-# this is triggered when submit button is clicked
-@app.route("/create", methods=['GET', 'POST'])
+@app.route("/create", methods=['POST'])
 def create():
-    if 'cover_image' and 'original' in request.files:
-        cover_img = request.files['cover_image']
-        original_epub = request.files['original']
+    try:
+        cover_img = request.files.get('cover_image')
+        original_epub = request.files.get('original')
+        if not cover_img or not original_epub:
+            return "Please upload both the cover image and the original EPUB file.", 400
 
-        # epub processing starts:
-        # this works!!! that means uploaded stuff is not empty
+        # Read and process the original EPUB
         ori = epub.read_epub(original_epub)
-        ch_list = []
+        ori_title = ori.get_metadata('DC', 'title')[0][0]
+        ori_author = ori.get_metadata('DC', 'creator')[0][0]
 
-        # get metadata of original book
-        li_title = ori.get_metadata('DC', 'title')
-        ori_title = li_title[0][0]
-        li_author = ori.get_metadata('DC', 'creator')
-        ori_author = li_author[0][0]
-
-        # creating new book
+        # Create new EPUB
         book = epub.EpubBook()
-
-        # calculating identifier for new book
-        now = datetime.now()
-        dt_string = now.strftime("%d%m%Y%H%M%S")
-
-        # add metadata
-        book.set_identifier(dt_string)
+        book.set_identifier(datetime.now().strftime("%Y%m%d%H%M%S"))
         book.set_title(ori_title)
         book.set_language('en')
         book.add_author(ori_author)
-
-        # add cover image
         book.set_cover("image.jpg", cover_img.read())
 
-        # intro chapter
-        intro = "Blank Page"
-        c1 = epub.EpubHtml(title='Introduction', file_name='intro.xhtml', lang='hr')
-        c1.content=u'<html><head></head><body><p>'+intro+'</p></body></html>'
-
-        # about chapter
-        c2 = epub.EpubHtml(title='Cover Page', file_name='about.xhtml')
-        c2.content='<p>Cover Image</p><p><img src="image.jpg" alt="Cover Image"/></p>'
-
-        # add starting two chapters to the book
+        # Intro chapter
+        c1 = epub.EpubHtml(title='Introduction', file_name='intro.xhtml', lang='en')
+        c1.content = u'<html><head></head><body><p>Blank Page</p></body></html>'
         book.add_item(c1)
+
+        # About chapter
+        c2 = epub.EpubHtml(title='Cover Page', file_name='about.xhtml')
+        c2.content = '<p>Cover Image</p><img src="image.jpg" alt="Cover Image"/>'
         book.add_item(c2)
 
-        # adding content (both text and imgs) extracted from original book
-        i = 1
+        # Add original content
+        ch_list = []
         for item in ori.get_items():
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                name = item.get_name()
-                ch = epub.EpubHtml(title='Chapter '+str(i), file_name = name[5:])
+                ch = epub.EpubHtml(title='Chapter', file_name=item.get_name()[5:], lang='en')
                 ch.content = item.get_content()
                 ch_list.append(ch)
                 book.add_item(ch)
-                i = i + 1
-
-            if item.get_type() == ebooklib.ITEM_IMAGE:
-                b_img = item.get_content()
-                img_name = item.get_name()
-                img_id = item.get_id()
-                img_item = epub.EpubItem(uid=img_id, file_name=img_name, media_type='image/jpeg', content=b_img)
-                book.add_item(img_item)
-
-
-        # create table of contents
-        # - add manual link
-        # - add section
-        # - add auto created links to chapters
-
 
         book.toc = (epub.Link('intro.xhtml', 'Introduction', 'intro'), (epub.Section('Contents'), ch_list))
-
-        # add navigation files
-        # NOTE: Do not remove navigation, if you do then iBooks shows the epub as corrupt
         book.add_item(epub.EpubNcx())
         book.add_item(epub.EpubNav())
 
-
-        # define css style
-        style = '''
-    @namespace epub "http://www.idpf.org/2007/ops";
-    body {
-        font-family: Cambria, Liberation Serif, Bitstream Vera Serif, Georgia, Times, Times New Roman, serif;
-    }
-    h2 {
-         text-align: left;
-         text-transform: uppercase;
-         font-weight: 200;
-    }
-    ol {
-            list-style-type: none;
-    }
-    ol > li:first-child {
-            margin-top: 0.3em;
-    }
-    nav[epub|type~='toc'] > ol > li > ol  {
-        list-style-type:square;
-    }
-    nav[epub|type~='toc'] > ol > li > ol > li {
-            margin-top: 0.3em;
-    }
-    '''
-
-        # add css file
-        nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
-        book.add_item(nav_css)
-
-        # create spin, add cover page as first page
-        book.spine = ['cover', 'nav', c1, c2] + ch_list
-
-        epub.write_epub(os.path.join(app.config['UPLOAD_FOLDER'], ori_title+'_'+dt_string+'.epub'), book, {})
+        # Save the new EPUB
+        file_path = os.path.join(UPLOAD_FOLDER, f"{ori_title}_{book.get_identifier()}.epub")
+        epub.write_epub(file_path, book, {})
 
         @after_this_request
         def remove_file(response):
-            os.remove(app.config['UPLOAD_FOLDER']+'/'+ori_title+'_'+dt_string+'.epub')
+            try:
+                os.remove(file_path)
+            except Exception as e:
+                app.logger.error(f"Error removing file: {e}")
             return response
 
-        return send_from_directory(app.config['UPLOAD_FOLDER'], ori_title+'_'+dt_string+'.epub', as_attachment=True)
+        return send_from_directory(directory=UPLOAD_FOLDER, filename=os.path.basename(file_path), as_attachment=True)
 
-
-    else:
-        return 'Please upload all documents.'
-
+    except Exception as e:
+        app.logger.error(f"Failed to create or send EPUB: {e}")
+        return Response(f"An error occurred: {str(e)}", status=500)
 
 if __name__ == "__main__":
     app.run(debug=True)
